80849e714b12fdbd63dbf00a70624485b7290d72
Lucas Natan <lucasnatan.dev@gmail.com>
2023-06-04 10:39:53 -0400

    fixed ALLOWED HOSTS config

--- mystore/settings.py
+++ mystore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mycontacts-backend.herokuapp.com']
+ALLOWED_HOSTS = ['my-store-backend.herokuapp.com']
 
 # Application definition
 
